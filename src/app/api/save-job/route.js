38584bf6686f9df0d5e81e902f76427328188f36
Anirudh Chidambaram <anirudh.chida@gmail.com>
2025-07-01 23:22:13 +1000

    Token addition to the backend

--- src/app/api/save-job/route.js
+++ src/app/api/save-job/route.js
@@ -2,21 +2,44 @@ import clientPromise from "./utility";
 
 export async function POST(req) {
     try {
-        const body = await req.json();
-        const { jobData, userEmail } = body;
+
+        const authHeader = req.headers.get("authorization");
+        const token = authHeader?.split(" ")[1];
+
+        if (!token) {
+            return new Response(JSON.stringify({ message: "Missing Token" }), { status: 401, })
+        }
 
         const client = await clientPromise;
         const db = client.db("Oohaah");
+
+        const session = await db.collection("sessions").findOne({ token });
+        if (!session) {
+            return new Response(JSON.stringify({ message: "Invalid Token" }), { status: 401 });
+        }
+
+        const body = await req.json();
+        const { jobData } = body;
+        console.log(jobData);
+
         const collection = db.collection("jobs");
 
+        // updating the token and job data
         const result = await collection.insertOne({
-            userEmail,
+            token,
             jobData,
             savedAt: new Date(),
         });
 
+        //updating the last used  will use later I think
+        await collection.updateOne(
+            { token },
+            { $set: { lastUsedAt: new Date() } }
+        );
+
         return new Response(JSON.stringify({ message: "Saved", id: result.insertedId }), { status: 200 });
-    } catch (error) {
+    }
+    catch (error) {
         console.error("Error saving to MongoDB:", error);
         return new Response(JSON.stringify({ message: "Failed to save" }), { status: 500 });
     }
